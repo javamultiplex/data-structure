31563a09dae0dc45e13786b15fb4b87931ae28cb
Rohit Agarwal <programmingwithrohit@gmail.com>
2017-06-04 20:29:00 +0530

    Add files via upload

---
+++ linkedlist/SingleLinkedList.java
@@ -0,0 +1,218 @@
+public class SingleLinkedList {
+
+	private class Node {
+
+		private int data;
+		private Node next;
+
+		Node(int data) {
+
+			setData(data);
+			setNext(null);
+		}
+
+		public int getData() {
+			return data;
+		}
+
+		public void setData(int data) {
+			this.data = data;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+
+	}
+
+	private Node head;
+
+	private int size;
+
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	// Insert at the end.
+	public void insertAtEnd(int data) {
+
+		Node node = new Node(data);
+
+		if (isEmpty()) {
+			head = node;
+		} else {
+			Node current = head;
+			while (current.getNext() != null) {
+				current = current.getNext();
+			}
+			current.setNext(node);
+		}
+		size++;
+	}
+
+	// Insert at the beginning.
+	public void insertAtBeginning(int data) {
+
+		Node node = new Node(data);
+		if (!isEmpty()) {
+			node.setNext(head);
+		}
+		head = node;
+		size++;
+	}
+
+	// Insert at the middle.
+	public void insertAtMiddle(int data, int position) {
+
+		if (isEmpty()) {
+
+			if (position == 1) {
+				head = new Node(data);
+			} else {
+				System.out.println("Invalid position " + position + ".");
+				return;
+			}
+		} else {
+
+			Node node = new Node(data);
+			if (position == 1) {
+				node.setNext(head);
+				head = node;
+			} else if (position > 1 && position <= getSize()) {
+				Node current = head, prev = null;
+				int currentPosition = 1;
+				while (currentPosition != position) {
+					prev = current;
+					current = current.getNext();
+					currentPosition++;
+				}
+				prev.setNext(node);
+				node.setNext(current);
+			} else if (position == getSize() + 1) {
+				Node current = head;
+				while (current.getNext() != null) {
+					current = current.getNext();
+				}
+				current.setNext(node);
+			} else {
+				System.out.println("Invalid position " + position + ".");
+				return;
+			}
+
+		}
+		size++;
+
+	}
+
+	// Delete from the middle.
+	public void deleteFromMiddle(int position) {
+
+		if (isEmpty()) {
+			System.out.println("Linked list is empty.");
+			return;
+		} else {
+			if (position == 1) {
+				head = head.getNext();
+			} else if (position > 1 && position < getSize()) {
+				Node current = head, prev = null;
+				int currentPosition = 1;
+				while (currentPosition != position) {
+					prev = current;
+					current = current.getNext();
+					currentPosition++;
+				}
+				prev.setNext(current.getNext());
+			} else if (position == getSize()) {
+				if (head.getNext() == null) {
+					head = null;
+				} else {
+					Node current = head;
+					while (current.getNext().getNext() != null) {
+						current = current.getNext();
+					}
+					current.setNext(null);
+				}
+			} else {
+				System.out.println("Invalid position");
+				return;
+			}
+		}
+		size--;
+
+	}
+
+	// Delete from the beginning.
+	public void deleteFromBeginning() {
+
+		if (isEmpty()) {
+			System.out.println("Linked list is empty.");
+			return;
+		} else {
+			head = head.getNext();
+		}
+		size--;
+	}
+
+	// Delete from the end.
+	public void deleteFromEnd() {
+
+		if (isEmpty()) {
+			System.out.println("Linked list is empty.");
+			return;
+		} else {
+			if (head.getNext() == null) {
+				head = null;
+			} else {
+				Node current = head;
+				while (current.getNext().getNext() != null) {
+					current = current.getNext();
+				}
+				current.setNext(null);
+			}
+		}
+
+		size--;
+
+	}
+
+	// Delete full linked list.
+	public void delete() {
+
+		if (isEmpty()) {
+			System.out.println("Linked list is empty.");
+			return;
+		} else {
+			while (head != null) {
+				head = head.getNext();
+				size--;
+			}
+		}
+
+	}
+
+	public void display() {
+
+		if (isEmpty()) {
+
+			System.out.println("Linked list is empty.");
+			return;
+
+		} else {
+			Node current = head;
+			while (current != null) {
+				System.out.print(current.getData() + "->");
+				current = current.getNext();
+			}
+		}
+
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+}
